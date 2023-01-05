6651e2415d0cd70745d2101d15b2f2ec91d997bc
valdidev <iamvaldidev@gmail.com>
2023-01-05 19:33:16 +0100

    wip: signin component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const SignIn = () => {
+  const onSubmit = () => null;
+
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="form container">
+      <form
+        onSubmit={onSubmit}
+        className="d-flex flex-column align-items-center justify-content-center"
+      >
+        <h1 className="text-center">Sign in</h1>
+        <input className="form-control my-2" type="email" placeholder="Email" />
+        <input
+          className="form-control my-2"
+          type="password"
+          placeholder="Password"
+        />
+        <p className="text-danger">Email or Password incorrect</p>
+        <button className="btn bg-orange-rs w-100 my-4">Send</button>
+      </form>
+    </div>
+  );
+};
