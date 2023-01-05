7316723dc79e21f3e468b3c4f43eee929b5b5548
valdidev <iamvaldidev@gmail.com>
2023-01-05 19:34:09 +0100

    wip: signup component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export const SignUp = () => {
+  const onSubmit = () => null;
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="form container form-container d-flex flex-column align-items-center">
+      <form
+        onSubmit={onSubmit}
+        className="d-flex flex-column align-items-center justify-content-center"
+      >
+        <h1 className="text-center">Sign up</h1>
+        <input className="form-control my-2" type="email" placeholder="Email" />
+        <input
+          className="form-control my-2"
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          className="form-control my-2"
+          type="password"
+          placeholder="Confirm password"
+        />
+        <button className="btn bg-orange-rs w-100 my-4">Send</button>
+      </form>
+    </div>
+  );
+};
