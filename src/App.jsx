3c729312228c862dd2c26901e26273c988b51821
valdidev <iamvaldidev@gmail.com>
2023-01-07 09:57:27 +0100

    wip: public routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import { Lobby } from "./components/Lobby/Lobby";
+import { AuthContextProvider } from "./contexts/authContext";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lobby" element={<Lobby />} />
-        <Route path="/private" element={<Home />} />
-        <Route path="/private/logout" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={}>
+            <Route path="/lobby" element={<Lobby />} />
+          </Route>
+
+          <Route path="/private">
+            <Route path="/private" element={<Home />} />
+            <Route path="/private/logout" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 };
