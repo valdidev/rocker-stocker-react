775711d251ea0dbf065362c7c9737db6636e5a08
valdidev <iamvaldidev@gmail.com>
2023-01-07 09:24:31 +0100

    wip: basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home/Home";
 import { Lobby } from "./components/Lobby/Lobby";
 
 export const App = () => {
-  return <Lobby />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lobby" element={<Lobby />} />
+        <Route path="/private" element={<Home />} />
+        <Route path="/private/logout" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
