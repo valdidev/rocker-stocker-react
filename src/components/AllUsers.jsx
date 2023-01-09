8a456a588c06a704579c2e25c6ceb2e400c53221
valdidev <iamvaldidev@gmail.com>
2023-01-09 13:04:17 +0100

    feat: all users table

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -1,9 +1,60 @@
-import '../index.css'
+import { useEffect, useState } from "react";
+import { axiosGet } from "../api/axios";
+import { Spinner } from "../common/Spinner";
+import "../index.css";
 
 export const AllUsers = () => {
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const userLogged = JSON.parse(localStorage.getItem("RS_USER"));
+
+  const userJwt = userLogged.jwt;
+
+  useEffect(() => {
+    try {
+      setIsLoading(true);
+      axiosGet("user/all", "", userJwt).then((data) => {
+        setUsers(data.data);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setUsers(null);
+      setIsLoading(false);
+      console.log(error);
+    }
+  }, []);
+
+  if (!users && isLoading) {
+    return <Spinner />;
+  }
+
   return (
-    <div className='contentDesign'>
-        <h1>AllUsers</h1>
-    </div>
-  )
-}
+    <table className="table">
+      <thead>
+        <tr>
+          {/* <th>Id #</th> */}
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Position</th>
+          <th>Active</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users?.map((user) => {
+          return (
+            <tr key={user.id}>
+              {/* <td data-label="id">{user.id}</td> */}
+              <td data-label="name">{user.name}</td>
+              <td data-label="email">{user.email}</td>
+              <td data-label="phone">{user.phone}</td>
+              <td data-label="position">{(user.rolId === 1) ? ('Manager') : ('Employee')}</td>
+              <td data-label="active">{(user.isActive) ? ('Active') : ('Block')}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
