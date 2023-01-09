3d88ce3ee0889869df56ac1bec5b8cc5b5a1caec
valdidev <iamvaldidev@gmail.com>
2023-01-09 22:09:29 +0100

    feat: reducer of shopping cart

---
+++ src/reducers/shoppingReducer.js
@@ -0,0 +1,27 @@
+import { TYPES } from "../actions/shoppingAction";
+
+export const shoppingInitialState = {
+    cart: []
+};
+
+export const shoppingReducer = (state, action) => {
+    switch (action.type) {
+        case TYPES.ADD_ONE_TO_CART: {
+
+        }
+
+        case TYPES.REMOVE_ONE_FROM_CART: {
+
+        }
+
+        case TYPES.REMOVE_ALL_FROM_CART: {
+
+        }
+
+        case TYPES.CLEAR_CART: {
+
+        }
+
+        default: return state;
+    };
+};
