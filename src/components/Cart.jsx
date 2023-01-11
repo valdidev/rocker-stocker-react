e2b5fa90193a4bc19c2c0d3958ce07b2e3326471
valdidev <iamvaldidev@gmail.com>
2023-01-11 19:21:53 +0100

    wip: demo design to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,28 @@
 import { useShopContext } from "../contexts/ShopContext";
 import "../index.css";
 
-export const Cart = () => {
+import "./cart.css";
 
-  const { cart, testeo } = useShopContext();
+export const Cart = () => {
+  const { cart, total } = useShopContext();
 
   return (
     <div className="contentDesign">
-      <h2>Cart</h2>
-      <h3>{testeo}</h3>
+      <h1>CART</h1>
+      <h2>Products</h2>
+      <ul>
+        {cart?.map((item, index) => (
+          <li key={index}>
+            ARTICLE ID: {item.articleId}. X {item.quantity}
+            <button className="mx-2 my-1 btn btn-success" onClick={() => console.log('++++')}>+</button>
+            <button className="mx-2 my-1 btn btn-warning" onClick={() => console.log('----!')}>-</button>
+            <button className="mx-2 my-1 btn btn-danger" onClick={() => console.log('delete')}>x</button>
+          </li>
+        ))}
+      </ul>
+      <h5>TOTAL: {total}</h5>
+      <button className="btn mx-2 btn-success" onClick={() => console.log('selling...')}>SELL</button>
+      <button className="btn mx-2 btn-danger" onClick={() => console.log('emptying...')}>EMPTY</button>
     </div>
   );
 };
