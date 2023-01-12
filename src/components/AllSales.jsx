a1db6d387c91fadf1f5e222185a0a996e77edab2
valdidev <iamvaldidev@gmail.com>
2023-01-12 22:03:10 +0100

    feat: all sales can get details

--- src/components/AllSales.jsx
+++ src/components/AllSales.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { CgDetailsMore } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
 import { axiosGet } from "../api/axios";
 import { Spinner } from "../common/Spinner";
 import "../index.css";
@@ -6,6 +8,8 @@ import "../index.css";
 export const AllSales = () => {
   const [sales, setSales] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const userLogged = JSON.parse(localStorage.getItem("RS_USER"));
 
@@ -37,6 +41,7 @@ export const AllSales = () => {
           <th>Date</th>
           <th>Employee #</th>
           <th>Total €</th>
+          <th>Details</th>
         </tr>
       </thead>
       <tbody>
@@ -47,6 +52,14 @@ export const AllSales = () => {
               <td data-label="date">{sale.date.split("T")[0]}</td>
               <td data-label="employeeId">{sale.userId}</td>
               <td data-label="total">{sale.total}</td>
+              <td data-label="Details">
+                  <div
+                    className="btn btn-info"
+                    onClick={() => navigate(`/private/sale/details/${sale.id}`)}
+                  >
+                    <CgDetailsMore />
+                  </div>
+                </td>
             </tr>
           );
         })}
