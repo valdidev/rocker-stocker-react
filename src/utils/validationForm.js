7466f0f413d80d40d2dfb187d12063544050b8b9
valdidev <iamvaldidev@gmail.com>
2023-01-19 21:06:47 +0100

    feat: validation file regex

--- src/utils/validationForm.js
+++ src/utils/validationForm.js
@@ -1,26 +1,72 @@
 export const validationsForm = (form) => {
-    let errors = {};
+
+    let errors = {
+        name: "",
+        surname: "",
+        phone: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    };
 
     let regexText = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-    let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    let regexEmail = /^(\w+[/./-]?){1,}@[rockerstocker]+\.com$/;
     let regexPhone = /^[0-9]{9}$/;
+    let regexPassword = /^.{8,}$/;
 
-    if (!form.name.trim()) {
+    // NAME
+    if (!form?.name?.trim()) {
         errors.name = "Name field is required";
     } else if (!regexText.test(form.name.trim())) {
-        errors.name = "Only accepts letters and white spaces";
+        errors.name = "Name only accepts letters";
+
     }
 
-    if (!form.surname.trim()) {
-        errors.surname = "Surname field is required";
-    } else if (!regexText.test(form.surname.trim())) {
-        errors.surname = "Only accepts letters and white spaces";
+    // SURNAME
+    if (Object.hasOwn(form, "surname")) {
+        if (!form?.surname?.trim()) {
+            errors.surname = "Surname field is required";
+
+        } else if (!regexText.test(form.surname.trim())) {
+            errors.surname = "Surname only accepts letters";
+
+        }
     }
 
-    if (!form.phone.trim()) {
-        errors.phone = "Phone field is required";
-    } else if (!regexPhone.test(form.phone.trim())) {
-        errors.phone = "Phone field only accepts 9 numbers";
+    // EMAIL
+    if (!form?.email?.trim()) {
+        errors.email = "Email field is required";
+
+    } else if (!regexEmail.test(form.email.trim())) {
+        errors.email = "Email must to be like username@rockerstocker.com";
+    }
+
+    // PASSWORD
+    if (!form?.password?.trim()) {
+        errors.password = "Password field is required";
+
+    } else if (!regexPassword.test(form.password.trim())) {
+        errors.password = "Password must be at least 8 characters";
+
+    }
+
+    // CONFIRM PASSWORD
+    if (!form?.confirmPassword?.trim()) {
+        errors.confirmPassword = "Confirm password field is required";
+
+    } else if (form?.password !== form?.confirmPassword) {
+        errors.confirmPassword = "Passwords do not match";
+
+    }
+
+    // PHONE
+    if (Object.hasOwn(form, "phone")) {
+        if (!form?.phone?.trim()) {
+            errors.phone = "Phone field is required";
+
+        } else if (!regexPhone.test(form.phone.trim())) {
+            errors.phone = "Phone field only accepts 9 numbers";
+        }
     }
 
     return errors;
